07b8ce99ce370f045b9a19564a35de0cc59859c7
Ootopi <ootopistreams@gmail.com>
2022-01-07 20:15:40 +0800

    Added drunk descriptions

--- index.js
+++ index.js
@@ -28,8 +28,6 @@ async function get_access_token() {
     .then(res => res?.access_token)
 }
 
-get_access_token()
-
 async function get_user(user_name) {
     const token = await get_access_token()
     let user = await cached_user(user_name)
@@ -79,8 +77,55 @@ async function cache_user(user) {
     return user
 }
 
+function rand_int(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function neighbouring_key(key) {
+    const is_upper = key === key.toUpperCase()
+    key = key.toLowerCase()
+    const keys = [
+        'qwertyuiop',
+        'asdfghjkl;',
+        'zxcvbnm,./'
+    ]
+
+    let row = -1
+    let col = -1
+
+    for(let r = 0; r < keys.length; r++) {
+        const i = keys[r].indexOf(key)
+        if(i == -1) continue
+        row = r
+        col = i
+    }
+
+    if(row == -1 || col == -1) return key
+    let row_min = row == 0 ? 0 : -1
+    let row_max = row == 2 ? 0 : 1
+    let rand_row = row + rand_int(row_min, row_max)
+    let col_min = col == 0 ? 0 : -1
+    let col_max = col == (keys[0].length - 1) ? 0 : 1
+    let rand_col = col + rand_int(col_min, col_max)
+
+    key = keys[rand_row][rand_col]
+
+    return is_upper ? key.toUpperCase() : key
+}
+
+function drunkenify(text, drunk_factor = 0.9) {
+    return Array.from(text).map(x => {
+        if(Math.random() <= drunk_factor) return x
+        const r = Math.random()
+        if(x == ' ') return r <= 0.5 ? '' : '  '
+        return r < 0.05 ? '' : r < 0.4 ? neighbouring_key(x) : r < 0.8 ? x.toUpperCase() : x.repeat(rand_int(0, 4))
+    }).join('')
+}
+
 app.get('/twitch/user/:user', (req, res) => {
+    console.log(req.params)
     if(!req.params.user) return
+    console.log(res)
     get_user(req.params.user).then(user => res.send(user))
 })
 app.get('/twitch/user/:user/description', (req, res) => {
@@ -91,5 +136,13 @@ app.get('/twitch/user/:user/force_update', (req, res) => {
     if(!req.params.user) return
     request_user(req.params.user).then(user => res.send(user))
 })
-
+app.get('/twitch/user/:user/broadcaster_type', (req, res) => {
+    if(!req.params.user) return
+    request_user(req.params.user).then(user => res.send(user?.broadcaster_type))
+})
+app.get('/twitch/user/:user/drunk_description/:factor', (req, res) => {
+    if(!req.params.user) return
+    request_user(req.params.user)
+        .then(user => res.send(drunkenify(user?.description, req.params.factor)))
+})
 app.listen(PORT)
